b9de8623ecc4049588f4fc0219ded706e73d3c9e
muktadiranik <muktadiranik@gmail.com>
2024-03-01 18:48:10 +0600

    multilanguage feature implemented

--- src/App.js
+++ src/App.js
@@ -115,27 +115,27 @@ function App() {
         </div>
         <div>
           <div className="mb-3">
-            <h5 className="form-label">Phone</h5>
+            <h5 className="form-label">Phone(মোবাইল নাম্বার)</h5>
             <input type="text" className="form-control" placeholder="Phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
           </div>
           <div className="mb-3">
-            <h5 className="form-label">Description</h5>
+            <h5 className="form-label">Description(বর্ণনা)</h5>
             <textarea placeholder="Description" cols="40" rows="5" className="form-control" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
           </div>
           <div className="mb-3">
-            <h5 className="form-label">Doctor's advice</h5>
+            <h5 className="form-label">Doctor's advice(ডাক্তারের পরামর্শ)</h5>
             <textarea placeholder="Doctor's advice" cols="40" rows="5" className="form-control" value={doctorsAdvice} onChange={(e) => setDoctorsAdvice(e.target.value)}></textarea>
           </div>
           <div className="mb-3">
-            <h5 className="form-label">Diagonosis</h5>
+            <h5 className="form-label">Diagonosis(রোগ নির্ণয়)</h5>
             <textarea placeholder="Diagonosis" cols="40" rows="5" className="form-control" value={diagonosis} onChange={(e) => setDiagonosis(e.target.value)}></textarea>
           </div>
           <div className="mb-3">
-            <h5 className="form-label">Treatment</h5>
+            <h5 className="form-label">Treatment(চিকিৎসা)</h5>
             <textarea placeholder="Treatment" cols="40" rows="5" className="form-control" value={treatment} onChange={(e) => setTreatment(e.target.value)}></textarea>
           </div>
           <div className="mb-3">
-            <h5 className="form-label">Diseases</h5>
+            <h5 className="form-label">Diseases(রোগসমূহ)</h5>
             {diseases &&
               diseases.map((disease) => (
                 <div className="form-check" key={disease.id}>
@@ -147,7 +147,7 @@ function App() {
               ))}
           </div>
           <div className="mb-3">
-            <h5 className="form-label">Images</h5>
+            <h5 className="form-label">Images(ছবিসমূহ)</h5>
             <div className="container d-flex justify-content-center align-items-center">
               <div className="text-center m-3">
                 <Dropzone onDrop={handleDrop}>
@@ -156,6 +156,7 @@ function App() {
                       <div {...getRootProps()} style={dropzoneStyles}>
                         <input {...getInputProps()} />
                         <p>Drag and drop some files here, or click to select files</p>
+                        <p>এখানে কিছু ফাইল টেনে আনুন এবং ড্রপ করুন অথবা ফাইল নির্বাচন করতে ক্লিক করুন</p>
                       </div>
                     </section>
                   )}
@@ -166,7 +167,7 @@ function App() {
           </div>
           {images.length > 0 && (
             <div className="mt-3">
-              <h5>Selected Images:</h5>
+              <h5>Selected Images(নির্বাচিত ছবিসমূহ):</h5>
               <div className="row justify-content-center">
                 {images.map((image, index) => (
                   <div className="col-md-3" key={index}>
@@ -183,7 +184,7 @@ function App() {
           )}
           <div className="text-center mb-3">
             <button type="button" className="btn btn-primary btn-lg" onClick={handleSubmit}>
-              Submit
+              Submit(জমা দিন)
             </button>
           </div>
         </div>
